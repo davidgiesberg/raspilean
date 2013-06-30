76a39aad30067869404667036ba7e22b5385c46e
David Giesberg <git@davidgiesberg.net>
2013-06-30 15:04:08 -0500

    Show currently playing track on page

--- raspilean.js
+++ raspilean.js
@@ -1,17 +1,21 @@
-var printCurrentTrack = function (track) {
-    if (track) {
-        console.log("Currently playing:", track.name, "by",
-            track.artists[0].name, "from", track.album.name);
-    } else {
-        console.log("No current track");
-    }
+var formattedCurrentTrack = function (track) {
+  return track.name + " by " + track.artists[0].name + " from " + track.album.name;
 };
 
 var getAndPrintCurrentTrack = function () {
-  console.log ("Getting Current Track")
-  mopidy.playback.getCurrentTrack().then(
-    printCurrentTrack, console.error.bind(console))
+  console.log ("Getting Current Track");
+  mopidy.playback.getCurrentTrack().then( function (track) {
+    console.log("Now Playing: ", formattedCurrentTrack(track));
+  })
+};
+
+var updateNowPlaying = function () {
+ // console.log ("Getting Current Track");
+  mopidy.playback.getCurrentTrack().then( function (track) {
+    document.getElementById("current_track").innerHTML = "Now Playing: " + formattedCurrentTrack(track);
+  })
 };
 
 var mopidy = new Mopidy();             // Connect to server
-mopidy.on("event:trackPlaybackStarted", function() { getAndPrintCurrentTrack() });
+//mopidy.on("event:trackPlaybackStarted", function() { getAndPrintCurrentTrack() });
+mopidy.on("event:trackPlaybackStarted", function() { updateNowPlaying() });
