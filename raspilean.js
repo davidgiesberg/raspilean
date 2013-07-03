c0c817ce9e8880e1e6c3a8703b12473b1fc00942
David Giesberg <git@davidgiesberg.net>
2013-07-02 22:24:01 -0500

    Basic playback controls. Closes #1

--- raspilean.js
+++ raspilean.js
@@ -19,3 +19,28 @@ var updateNowPlaying = function () {
 var mopidy = new Mopidy();             // Connect to server
 mopidy.on("state:online", function() { updateNowPlaying() });
 mopidy.on("event:trackPlaybackStarted", function() { updateNowPlaying() });
+
+window.onload = function() {
+
+  var play = document.getElementById("play");
+  var pause = document.getElementById("pause");
+  var previous = document.getElementById("previous");
+  var next = document.getElementById("next");
+
+  play.onclick = function() {
+    mopidy.playback.play();
+    return false;
+  }
+  pause.onclick = function() {
+    mopidy.playback.pause();
+    return false;
+  }
+  previous.onclick = function() {
+    mopidy.playback.previous();
+    return false;
+  }
+  next.onclick = function() {
+    mopidy.playback.next();
+    return false;
+  }
+}
